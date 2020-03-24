25c987ed9e9b7d0d204046a86f087b76d1c14877
Martin <martin.setina@seznam.cz>
2020-03-24 22:53:01 +0100

    late night, adapting to new game state object, still need to receive active player from the server + information about the players from administration to show

--- resources/js/Components/PlayerList.jsx
+++ resources/js/Components/PlayerList.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 
 class PlayerList extends Component {
+  constructor(props){
+    super(props)
+  }
 
   render(){
     return (
       <div>
         <ul className="list-group mb-4 ml-5">
-          <li className="list-group-item active">Player 1</li>
-          <li className="list-group-item">Player 2</li>
-          <li className="list-group-item">Player 3</li>
-          <li className="list-group-item">Player 4</li>
-          <li className="list-group-item">Player 5</li>
+          {
+            this.props.turns.map((player, index) => {
+              if(player === this.props.activePlayer) {
+                return (
+                  <li key={index} className="list-group-item active">{player}</li>
+                )
+              } else {
+                return (
+                  <li key={index} className="list-group-item">{player}</li>
+                )
+              }
+            })
+
+          }
         </ul>        
       </div>
     )
