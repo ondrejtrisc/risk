c022f0dff2b52a1c28eac9e8d0cc182653e88c13
laszlovladi <laszlo.vladi@gmail.com>
2020-04-02 18:15:03 +0200

    rules

--- resources/views/description.blade.php
+++ resources/views/description.blade.php
@@ -40,7 +40,14 @@
                         <div class="col">
                             {{-- <h2 class="row justify-content-center">Hi {{ Auth::user()->name }}, let's play!)</h2> --}}
                             <div class="row justify-content-center">
-                              Here will be some description.
+                              Risk is a strategy board game of diplomacy, conflict and conquest for two to six players. 
+                              The standard version is played on a board depicting a political map of Earth, divided into 
+                              forty-two territories, which are grouped into six continents. Turn rotates among players who 
+                              control armies of playing pieces with which they attempt to capture territories from other 
+                              players, with results determined by dice rolls. Players may form and dissolve alliances during 
+                              the course of the game. The goal of the game is to occupy every territory on the board and in 
+                              doing so, eliminate the other players. The game can be lengthy, requiring several hours to 
+                              multiple days to finish.                            
                             </div>
                         </div>
                     {{-- @endauth --}}
